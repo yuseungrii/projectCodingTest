4237178a9fe4b345c1056658cd14c4327e54188c
yuseungri <siso1210@gmail.com>
2021-04-27 17:57:34 +0900

    프로그래머스 모의고사2

--- src/com/company/node/Programmers42840.js
+++ src/com/company/node/Programmers42840.js
@@ -6,7 +6,7 @@ function solution(answers) {
         [1,2,3,4,5],
         [2,1,2,3,2,4,2,5],
         [3,3,1,1,2,2,4,4,5,5]
-    ]
+    ];
 
     // 정답배열을 돌면서 각자 맞춘 갯수 저장.
     for(let i=0; i<answers.length; i++){
@@ -23,6 +23,9 @@ function solution(answers) {
         if(point[j] > max)
             max = point[j];
     }
+    console.log(point, 'point');
+    console.log(max, 'max');
+
     //처음부터 순서대로 돌면서 point가 max와 같으면 answer에 push.
     for(let k=0; k<point.length; k++){
         if(max===point[k])
@@ -31,6 +34,30 @@ function solution(answers) {
     return answer;
 }
 
+function solution2(answers) {
+    let anser = [];
+    let list = [
+        [1,2,3,4,5],
+        [2,1,2,3,2,4,2,5],
+        [3,3,1,1,2,2,4,4,5,5]
+    ];
 
-console.log(solution([1, 2, 3, 4, 5]));
-console.log(solution([1, 3, 2, 4, 2]));
+    let point = [0,0,0];
+    for(let i=0; i < list.length;i++) {
+        point[i] = answers.filter( (a, idx) => a === list[i][idx%list[i].length] ).length;
+    }
+
+    let max = Math.max(point[0], point[1], point[2]);
+
+    if(point[0] === max) anser.push(1);
+    if(point[1] === max) anser.push(2);
+    if(point[2] === max) anser.push(3);
+
+    return anser;
+}
+
+
+console.log(solution2([1, 3, 2, 4, 2]));
+
+// console.log(solution([1, 2, 3, 4, 5]));
+// console.log(solution([1, 3, 2, 4, 2]));
